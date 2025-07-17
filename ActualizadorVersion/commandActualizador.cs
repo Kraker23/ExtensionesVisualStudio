9cf35932352865102a7515c40474be7bc3845417
Cristian J Ramirez <cristian.javier.ramirez@es.tuv.com>
2025-07-17 17:06:42 +0200

    Crear proyecto ActualizadorVersion

---
+++ ActualizadorVersion/commandActualizador.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+using EnvDTE;
+using EnvDTE80;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using VSLangProj;
+using System.Collections;
+
+namespace ActualizadorVersion
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class commandActualizador
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("58bc734d-5928-426d-bb8b-540b9feeb2b6");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="commandActualizador"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private commandActualizador(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static commandActualizador Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in commandActualizador's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new commandActualizador(package, commandService);
+        }
+
+        string Cambios = string.Empty;
+        string NewVersion = string.Empty;
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            try
+            {
+                frmVersion frm = new frmVersion(NewVersion);
+                if (DialogResult.OK == frm.ShowDialog())
+                {
+                    //string NewVersion = "2.0.3";
+                    NewVersion = frm.version;
+                    Hashtable mapHierarchies = new Hashtable();
+                    Cambios = string.Empty;
+                    DTE2 dte2 = Package.GetGlobalService(typeof(DTE)) as DTE2;
+                    var sol = dte2.Solution;
+                    //var projs = sol.Projects;
+                    Projects projs = sol.Projects;
+
+                    AddMessage($"Solucion => {sol.FullName} ({projs.Count})");
+                    RecursivaProjectos(projs);
+
+                    ThreadHelper.ThrowIfNotOnUIThread();
+                    //string messageT = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+                    string titleT = $"Actualizada la version a {NewVersion}";
+                    // Show a message box to prove we were here
+                    VsShellUtilities.ShowMessageBox(
+                        this.package,
+                        Cambios,
+                        titleT,
+                        OLEMSGICON.OLEMSGICON_INFO,
+                        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                }
+            }
+            catch (Exception ex)
+            {
+                string titleT = $"ERROR al Actualizada la version";
+                // Show a message box to prove we were here
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    ex.ToString(),
+                    titleT,
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+
+        private void RecursivaProjectos(IEnumerable projs)
+        {
+            foreach (var proj in projs)
+            {
+                var project = proj as Project;
+                if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    AddMessage($"Carpeta => {project.Name} ");
+                    var projInFolders = project.ProjectItems;
+
+                    var innerProjects = GetSolutionFolderProjects(project);
+                    RecursivaProjectos(innerProjects);
+                }
+                else //if (project.Kind == PrjKind.prjKindCSharpProject)
+                {
+                    AddMessage($"Projecto => {project.Name} ");
+                    if (project.Properties != null)
+                    {
+                        foreach (Property property in project.Properties)
+                        {
+                            string name = property.Name;
+                            if (property.Value != null && property.Name == "Version")
+                            {
+                                string version_OLD = property.Value.ToString();
+                                property.Value = NewVersion;
+                                string version_NEW = property.Value.ToString();
+                                AddMessage($"Version {version_OLD} => {version_NEW}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Project> GetSolutionFolderProjects(Project project)
+        {
+            List<Project> projects = new List<Project>();
+            var y = (project.ProjectItems as ProjectItems).Count;
+            for (var i = 1; i <= y; i++)
+            {
+                var x = project.ProjectItems.Item(i).SubProject;
+                var subProject = x as Project;
+                if (subProject != null)
+                {
+                    projects.Add(subProject);
+                    //Carried out work and added projects as appropriate
+                }
+            }
+
+            return projects;
+        }
+
+        private void AddMessage(string message)
+        {
+            Cambios = $"{Cambios}{message} {Environment.NewLine}";
+        }
+    }
+}
---
+++ ActualizadorVersion/frmVersion.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ActualizadorVersion
+{
+    partial class frmVersion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(15, 25);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(131, 20);
+            this.txtVersion.TabIndex = 0;
+            this.txtVersion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVersion_KeyPress);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(12, 9);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(134, 13);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = "Introduce la nueva Versión";
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Location = new System.Drawing.Point(82, 51);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(64, 23);
+            this.btAceptar.TabIndex = 2;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(15, 51);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(64, 23);
+            this.btCancelar.TabIndex = 1;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(22, 77);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(113, 13);
+            this.lblError.TabIndex = 5;
+            this.lblError.Text = "Error versión no Valida";
+            this.lblError.Visible = false;
+            // 
+            // frmVersion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(156, 97);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.txtVersion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmVersion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nueva Versión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Label lblError;
+    }
+}
